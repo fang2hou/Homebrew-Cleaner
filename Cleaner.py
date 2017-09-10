69a932137c36de36caf4da1a8ffaff209c0cbdea
Fang2hou <houshuu@live.jp>
2017-09-10 11:16:29 +0800

    Add Cleaner

---
+++ Cleaner.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# Homebrew Cleaner
+# Author: Fang2hou
+
+import commands
+import re
+
+ignoreList = [
+    "aria2",
+    "zsh",
+    "you-get",
+    "wget",
+    "go",
+    "node",
+    "python",
+    "python3",
+    "vim",
+    "ruby",
+]
+
+def IsListed(formula):
+    for listedFormula in ignoreList:
+        if listedFormula == formula:
+            return True
+    return False
+
+def SetDeps(formula):
+    depString = commands.getoutput('brew deps ' + formula)
+    depList = depString.split("\n")
+    if depList != [""]:
+        for ignoreDeps in depList:
+            if not IsListed(ignoreDeps):
+                ignoreList.append(ignoreDeps)
+                SetDeps(ignoreDeps)
+
+def GetAllFormulae():
+    formulaString = commands.getoutput('brew list')
+    formulaList = re.split("\t|\n|", formulaString)
+    return formulaList
+
+def Main():
+    loopList = ignoreList[:]
+    for formula in loopList:
+        SetDeps(formula)
+
+    loopList = GetAllFormulae()
+
+    for formula in loopList:
+        if not IsListed(formula):
+            commands.getoutput('brew uninstall --ignore-dependencies ' + formula)
+
+Main()
