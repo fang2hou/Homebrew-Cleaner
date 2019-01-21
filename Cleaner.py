8ee5faf0d54417df76e0a9a80a274f0285848f28
Zhou Fang <houshuu@live.jp>
2019-01-22 01:14:33 +0900

    Rewrite cleaner

--- Cleaner.py
+++ Cleaner.py
@@ -1,16 +1,12 @@
-#!/usr/bin/python
-# Homebrew Cleaner
-# Author: Fang2hou
-
+# This Python file uses the following encoding: utf-8
 import subprocess
 import re
 
-ignoreList = [
+default_ignore_formulae = [
     "telnet",
     "zsh",
-    "you-get",
-    "ffmpeg",
-    "libav",
+    "fish",
+    "clang-format",
     "wget",
     "go",
     "node",
@@ -18,42 +14,60 @@ ignoreList = [
     "python3",
     "vim",
     "imagemagick",
-    "gnuplot",
-    "lua",
+    "gnuplot"
 ]
 
-def IsListed(formulaName):
-    for listedFormula in ignoreList:
-        if listedFormula == formulaName:
+class HomebrewCleaner():
+    def __init__(self, ignore=None):
+        self.ignore_formulae = ignore if ignore else default_ignore_formulae
+        self.get_all_formulae()
+
+    def is_listed(self, name_of_formula):
+        if name_of_formula in self.formulae:
             return True
-    return False
+        else:
+            return False
 
-def SetDeps(formulaName):
-    depString = subprocess.check_output(["brew", "deps", formulaName])
-    depString = depString.decode("UTF-8").rstrip()
-    depList   = depString.split("\n")
-    print(depList)
-    if len(depList) > 1:
-        for ignoreDeps in depList:
-            if not IsListed(ignoreDeps):
-                ignoreList.append(ignoreDeps)
-                SetDeps(ignoreDeps)
+    def get_all_formulae(self):
+        system_string = subprocess.check_output(["brew", "list"]).rstrip()
+        formulae = re.split(r"\t|\n", system_string)
+        self.formulae = formulae
 
-def GetAllFormulae():
-    formulaString = subprocess.check_output(["brew", "list"])
-    formulaString = formulaString.decode("UTF-8").rstrip()
-    formulaList   = re.split(r"\t|\n", formulaString)
-    return formulaList
+    def ignore_dependencies(self, name_of_formula):
+        system_string = subprocess.check_output(["brew", "deps", name_of_formula])
+        dependencies = system_string.rstrip().split("\n")
+        
+        if dependencies[0] != "":
+            next_dependencies = []
 
-def Delete():
-    for formula in ignoreList:
-        SetDeps(formula)
+            for dependency in dependencies:
+                if self.is_listed(dependency):
+                    self.formulae.remove(dependency)
+                    next_dependencies.append(dependency)
 
-    formulae = GetAllFormulae()
+            for dependency in next_dependencies:
+                self.ignore_dependencies(dependency)
 
-    for formula in formulae:
-        if not IsListed(formula):
-            # subprocess.run(['brew uninstall --ignore-dependencies ' + formula])
-            print("%s" % formula)
-Delete()
-print("Done!")
+    def clean(self):
+        print("\033[94mHomebrew Cleaner:\033[0m Start analyzing... 🔍")
+
+        for formula in self.ignore_formulae:
+            if self.is_listed(formula):
+                self.formulae.remove(formula)
+                self.ignore_dependencies(formula)
+
+        print("================================================")
+
+        if len(self.formulae) != 0:
+            for formula in self.formulae: self.delete(formula)
+            print("\033[94mHomebrew Cleaner:\033[0m Done! ✅")
+        else:
+            print("\033[94mHomebrew Cleaner:\033[0m Nothing needs to be removed! 💯")
+
+    def delete(self, name_of_formula):
+       subprocess.check_output(["brew", "uninstall", "--ignore-dependencies", name_of_formula])
+       print("\033[94mHomebrew Cleaner:\033[0m \"%s\" has been deleted. ♻️" % name_of_formula)
+
+if __name__ == "__main__":
+    hc = HomebrewCleaner()
+    hc.clean()
